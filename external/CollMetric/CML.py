1d6bd6f8e00cf59c73813d2eecabbd94d2378cb6
XUEPAI <jmzhu.hk@gmail.com>
2020-12-28 17:39:10 +0800

    Update CollMetric

--- external/CollMetric/CML.py
+++ external/CollMetric/CML.py
@@ -1,11 +1,15 @@
 import functools
 import numpy
+import numpy as np
 import tensorflow as tf
 import toolz
+import os
 from tqdm import tqdm
+from datetime import datetime
 from evaluator import RecallEvaluator
 from sampler import WarpSampler
 from utils import citeulike, split_data
+from CML_utils import Evaluator, Monitor, parse_args, dataset_to_uimatrix
 
 
 def doublewrap(function):
@@ -30,7 +34,7 @@ def define_scope(function, scope=None, *args, **kwargs):
     A decorator for functions that define TensorFlow operations. The wrapped
     function will only be executed once. Subsequent calls to it will directly
     return the result so that operations are added to the graph only once.
-    The operations added by the function live within a tf.variable_scope(). If
+    The operations added by the function live within a tf.compat.v1.variable_scope(). If
     this decorator is used with arguments, they will be forwarded to the
     variable scope. The scope name defaults to the name of the wrapped
     function.
@@ -42,7 +46,7 @@ def define_scope(function, scope=None, *args, **kwargs):
     @functools.wraps(function)
     def decorator(self):
         if not hasattr(self, attribute):
-            with tf.variable_scope(name, *args, **kwargs):
+            with tf.compat.v1.variable_scope(name, *args, **kwargs):
                 setattr(self, attribute, function(self))
         return getattr(self, attribute)
 
@@ -106,9 +110,9 @@ class CML(object):
         self.cov_loss_weight = cov_loss_weight
 
 
-        self.user_positive_items_pairs = tf.placeholder(tf.int32, [None, 2])
-        self.negative_samples = tf.placeholder(tf.int32, [None, None])
-        self.score_user_ids = tf.placeholder(tf.int32, [None])
+        self.user_positive_items_pairs = tf.compat.v1.placeholder(tf.int32, [None, 2])
+        self.negative_samples = tf.compat.v1.placeholder(tf.int32, [None, None])
+        self.score_user_ids = tf.compat.v1.placeholder(tf.int32, [None])
 
 
         self.user_embeddings
@@ -121,12 +125,12 @@ class CML(object):
 
     @define_scope
     def user_embeddings(self):
-        return tf.Variable(tf.random_normal([self.n_users, self.embed_dim],
+        return tf.Variable(tf.random.normal([self.n_users, self.embed_dim],
                                             stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32))
 
     @define_scope
     def item_embeddings(self):
-        return tf.Variable(tf.random_normal([self.n_items, self.embed_dim],
+        return tf.Variable(tf.random.normal([self.n_items, self.embed_dim],
                                             stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32))
 
     @define_scope
@@ -162,7 +166,7 @@ class CML(object):
         loss = tf.constant(0, dtype=tf.float32)
         if self.feature_projection is not None:
             # the distance between feature projection and the item's actual location in the embedding
-            feature_distance = tf.reduce_sum(tf.squared_difference(
+            feature_distance = tf.reduce_sum(tf.math.squared_difference(
                 self.item_embeddings,
                 self.feature_projection), 1)
 
@@ -199,13 +203,13 @@ class CML(object):
         pos_items = tf.nn.embedding_lookup(self.item_embeddings, self.user_positive_items_pairs[:, 1],
                                            name="pos_items")
         # positive item to user distance (N)
-        pos_distances = tf.reduce_sum(tf.squared_difference(users, pos_items), 1, name="pos_distances")
+        pos_distances = tf.reduce_sum(tf.math.squared_difference(users, pos_items), 1, name="pos_distances")
 
         # negative item embedding (N, K, W)
         neg_items = tf.transpose(tf.nn.embedding_lookup(self.item_embeddings, self.negative_samples),
                                  (0, 2, 1), name="neg_items")
         # distance to negative items (N x W)
-        distance_to_neg_items = tf.reduce_sum(tf.squared_difference(tf.expand_dims(users, -1), neg_items), 1,
+        distance_to_neg_items = tf.reduce_sum(tf.math.squared_difference(tf.expand_dims(users, -1), neg_items), 1,
                                               name="distance_to_neg_items")
 
         # best negative item (among W negative samples) their distance to the user embedding (N)
@@ -221,7 +225,7 @@ class CML(object):
             # approximate the rank of positive item by (number of impostor / W per user-positive pair)
             rank = tf.reduce_mean(tf.cast(impostors, dtype=tf.float32), 1, name="rank_weight") * self.n_items
             # apply rank weight
-            loss_per_pair *= tf.log(rank + 1)
+            loss_per_pair *= tf.math.log(rank + 1)
 
         # the embedding loss
         loss = tf.reduce_sum(loss_per_pair, name="loss")
@@ -240,20 +244,20 @@ class CML(object):
 
     @define_scope
     def clip_by_norm_op(self):
-        return [tf.assign(self.user_embeddings, tf.clip_by_norm(self.user_embeddings, self.clip_norm, axes=[1])),
-                tf.assign(self.item_embeddings, tf.clip_by_norm(self.item_embeddings, self.clip_norm, axes=[1]))]
+        return [tf.compat.v1.assign(self.user_embeddings, tf.clip_by_norm(self.user_embeddings, self.clip_norm, axes=[1])),
+                tf.compat.v1.assign(self.item_embeddings, tf.clip_by_norm(self.item_embeddings, self.clip_norm, axes=[1]))]
 
     @define_scope
     def optimize(self):
         # have two separate learning rates. The first one for user/item embedding is un-normalized.
         # The second one for feature projector NN is normalized by the number of items.
         gds = []
-        gds.append(tf.train
-                   .AdagradOptimizer(self.master_learning_rate)
+        gds.append(tf.compat.v1.train
+                   .AdamOptimizer(self.master_learning_rate)
                    .minimize(self.loss, var_list=[self.user_embeddings, self.item_embeddings]))
         if self.feature_projection is not None:
-            gds.append(tf.train
-                       .AdagradOptimizer(self.master_learning_rate)
+            gds.append(tf.compat.v1.train
+                       .AdamOptimizer(self.master_learning_rate)
                        .minimize(self.feature_loss / self.n_items))
 
         with tf.control_dependencies(gds):
@@ -266,69 +270,95 @@ class CML(object):
         # (1, N_ITEM, K)
         item = tf.expand_dims(self.item_embeddings, 0)
         # score = minus distance (N_USER, N_ITEM)
-        return -tf.reduce_sum(tf.squared_difference(user, item), 2, name="scores")
+        return -tf.reduce_sum(tf.math.squared_difference(user, item), 2, name="scores")
 
 
-BATCH_SIZE = 50000
-N_NEGATIVE = 20
-EVALUATION_EVERY_N_BATCHES = 30
-EMBED_DIM = 100
-
-
-def optimize(model, sampler, train, valid):
+def optimize(model, sampler, train, valid, max_steps, monitor, verbose):
     """
-    Optimize the model. TODO: implement early-stopping
+    Optimize the model.
     :param model: model to optimize
     :param sampler: mini-batch sampler
     :param train: train user-item matrix
     :param valid: validation user-item matrix
     :return: None
     """
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+    session_conf = tf.compat.v1.ConfigProto()
+    session_conf.gpu_options.allow_growth = True
+    sess = tf.compat.v1.Session(config=session_conf)
+    sess.run(tf.compat.v1.global_variables_initializer())
     if model.feature_projection is not None:
         # initialize item embedding with feature projection
-        sess.run(tf.assign(model.item_embeddings, model.feature_projection))
+        sess.run(tf.compat.v1.assign(model.item_embeddings, model.feature_projection))
 
     # sample some users to calculate recall validation
-    valid_users = numpy.random.choice(list(set(valid.nonzero()[0])), size=1000, replace=False)
+    valid_users = np.random.choice(list(set(valid.nonzero()[0])), size=1000, replace=False)
 
-    while True:
+    for step in range(max_steps):
         # create evaluator on validation set
-        validation_recall = RecallEvaluator(model, train, valid)
-        # compute recall on validate set
+        metrics_evaluator = Evaluator(model, train, valid)
+        
+        ## K=20
+        # compute metrics on validate set
+        valid_hitrates = []
         valid_recalls = []
-
+        valid_NDCGs = []
         # compute recall in chunks to utilize speedup provided by Tensorflow
         for user_chunk in toolz.partition_all(100, valid_users):
-            valid_recalls.extend([validation_recall.eval(sess, user_chunk)])
-        print("\nRecall on (sampled) validation set: {}".format(numpy.mean(valid_recalls)))
-        # TODO: early stopping based on validation recall
+            hitrates, recalls, NDCGs = metrics_evaluator.eval(sess, user_chunk, k=20)
+            valid_hitrates.extend(hitrates)
+            valid_recalls.extend(recalls)
+            valid_NDCGs.extend(NDCGs)
+        hitrate20 = np.mean(valid_hitrates)
+        recall20 = np.mean(valid_recalls)
+        ndcg20 = np.mean(valid_NDCGs)
+        
+        ## K=50
+        # compute metrics on validate set
+        valid_hitrates = []
+        valid_recalls = []
+        valid_NDCGs = []
+        # compute recall in chunks to utilize speedup provided by Tensorflow
+        for user_chunk in toolz.partition_all(100, valid_users):
+            hitrates, recalls, NDCGs = metrics_evaluator.eval(sess, user_chunk, k=50)
+            valid_hitrates.extend(hitrates)
+            valid_recalls.extend(recalls)
+            valid_NDCGs.extend(NDCGs)
+        hitrate50 = np.mean(valid_hitrates)
+        recall50 = np.mean(valid_recalls)
+        ndcg50 = np.mean(valid_NDCGs)
+        
+        # early stopping based on validation metrics
+        early_stop = monitor.update_monitor(hitrate20, recall20, ndcg20, hitrate50, recall50, ndcg50)
+        if early_stop:
+            print("%s early stop at step %d" % (datetime.now(), step))
+            return
 
         # train model
         losses = []
         # run n mini-batches
-        for _ in tqdm(range(EVALUATION_EVERY_N_BATCHES), desc="Optimizing..."):
+        for _ in tqdm(range(verbose), desc="Optimizing"):
             user_pos, neg = sampler.next_batch()
             _, loss = sess.run((model.optimize, model.loss),
                                {model.user_positive_items_pairs: user_pos,
                                 model.negative_samples: neg})
-
             losses.append(loss)
 
-        print("\nTraining loss {}".format(numpy.mean(losses)))
+        print("{} Step {}, training loss {}\n".format(datetime.now(), step, np.mean(losses)))
 
 
 if __name__ == '__main__':
-    # get user-item matrix
-    user_item_matrix, features = citeulike(tag_occurence_thres=5)
-    n_users, n_items = user_item_matrix.shape
-    # make feature as dense matrix
-    dense_features = features.toarray() + 1E-10
-    # get train/valid/test user-item matrices
-    train, valid, test = split_data(user_item_matrix)
-    # create warp sampler
-    sampler = WarpSampler(train, batch_size=BATCH_SIZE, n_negative=N_NEGATIVE, check_negative=True)
+    args = parse_args()
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    np.random.seed(2017)
+    random_seed = 2017
+    tf.compat.v1.set_random_seed(random_seed)
+    
+    print("%s reading dataset %s" % (datetime.now(), args.dataset))
+    train, valid = dataset_to_uimatrix(args.train_data, args.test_data)
+    n_users, n_items = train.shape
+    print("%s #users=%d, #items=%d" % (datetime.now(), n_users, n_items))
+    sampler = WarpSampler(train, batch_size=args.batch_size, n_negative=args.num_negative, check_negative=True)
 
     # WITHOUT features
     # Train a user-item joint embedding, where the items a user likes will be pulled closer to this users.
@@ -338,13 +368,16 @@ if __name__ == '__main__':
                 # set features to None to disable feature projection
                 features=None,
                 # size of embedding
-                embed_dim=EMBED_DIM,
+                embed_dim=args.embed_dim,
                 # the size of hinge loss margin.
-                margin=1.9,
+                margin=args.margin,
                 # clip the embedding so that their norm <= clip_norm
-                clip_norm=1,
+                clip_norm=args.clip_norm,
                 # learning rate for AdaGrad
-                master_learning_rate=0.1,
+                master_learning_rate=args.lr,
+                
+                # dropout_rate = 1 - keep_prob
+                dropout_rate=args.dropout,
 
                 # whether to enable rank weight. If True, the loss will be scaled by the estimated
                 # log-rank of the positive items. If False, no weight will be applied.
@@ -363,45 +396,10 @@ if __name__ == '__main__':
                 cov_loss_weight=1
                 )
 
-    #optimize(model, sampler, train, valid)
-
-    # WITH features
-    # In this case, we additionally train a feature projector to project raw item features into the
-    # embedding. The projection serves as "a prior" to inform the item's potential location in the embedding.
-    # We use a two fully-connected layers NN as our feature projector. (This model is much more computation intensive.
-    # A GPU machine is recommended)
-    model = CML(n_users,
-                n_items,
-                # enable feature projection
-                features=dense_features,
-                embed_dim=EMBED_DIM,
-                margin=2.0,
-                clip_norm=1.1,
-                master_learning_rate=0.1,
-                # the size of the hidden layer in the feature projector NN
-                hidden_layer_dim=512,
-                # dropout rate between hidden layer and output layer in the feature projector NN
-                dropout_rate=0.3,
-                # scale the output of the NN so that the magnitude of the NN output is closer to the item embedding
-                feature_projection_scaling_factor=1,
-                # the penalty to the distance between projection and item's actual location in the embedding
-                # tune this to adjust how much the embedding should be biased towards the item features.
-                feature_l2_reg=0.1,
-
-                # whether to enable rank weight. If True, the loss will be scaled by the estimated
-                # log-rank of the positive items. If False, no weight will be applied.
-
-                # This is particularly useful to speed up the training for large item set.
-
-                # Weston, Jason, Samy Bengio, and Nicolas Usunier.
-                # "Wsabie: Scaling up to large vocabulary image annotation." IJCAI. Vol. 11. 2011.
-                use_rank_weight=True,
-
-                # whether to enable covariance regularization to encourage efficient use of the vector space.
-                # More useful when the size of embedding is smaller (e.g. < 20 ).
-                use_cov_loss=False,
-
-                # weight of the cov_loss
-                cov_loss_weight=1
-                )
-    optimize(model, sampler, train, valid)
+    log_file = open('dataset_' + args.dataset + '_margin_' + str(args.margin) + '_lr_' + str(args.lr) + '.csv', "w")
+    monitor = Monitor(log_file=log_file)
+    optimize(model, sampler, train, valid, max_steps=args.max_steps, monitor=monitor, verbose=args.verbose)
+    print("%s close sampler, close and save to log file" % datetime.now())
+    log_file.close()
+    sampler.close() # important! stop multithreading
+    print("%s log file and sampler have closed" % datetime.now())
